df9fc650fe93756df52a7923aa39d06d084f805a
Avi <avi@aravindh.me>
2020-11-21 22:59:46 -0500

    adding line height using Tailwind

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import Layout from '../components/Layout/layout';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
+    <Layout className='leading-normal'>
       <Component {...pageProps} />
     </Layout>
   );
