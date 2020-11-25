c99c1ca4bf542489ad0166899f731f63098acc94
Avi <avi@aravindh.me>
2020-11-24 21:02:03 -0500

    Adding SegoeFont back

--- pages/lab.js
+++ pages/lab.js
@@ -42,26 +42,6 @@ const Lab = ({ projects }) => {
             <Post {...project} index={index} />
           ))}
         </div>
-
-        {/* <div className='w-4/5 md:w-3/5 mt-8 flex justify-center flex-row flex-wrap'>
-          <div className='filter backdrop-brightness w-full font-montserrat text-md md:text-xl justify-center'>
-            A collection of things I build out of my own interest. All of the
-            work I do in my day job is NDA restricted.
-          </div>
-          <div className='w-full font-montserrat mt-5 text-md md:text-xl'>
-            If you would like to discuss more about my experience, reach me at{' '}
-            <span className='text-blue-700'>
-              <a href='mailto:avi@aravindh.me' target='_blank'>
-                avi@aravindh.me
-              </a>
-            </span>
-          </div>
-        </div>
-        <div className='w-full mt-8 md:flex md:flex-wrap md:w-3/5 md:justify-center'>
-          {projects.map((project, index) => (
-            <Post {...project} index={index} />
-          ))}
-        </div> */}
       </div>
     </Fragment>
   );
